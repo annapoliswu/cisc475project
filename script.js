5ac8a723b777a34ee4edd5bb7cb4c9cb6c275d43
daveb4 <53497062+daveb4@users.noreply.github.com>
2020-10-02 03:30:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,9 +11,32 @@ $("#submitButton").click(function () {
     */
 
     d3.csv(fileURL).then(function(data) {
+	/*
         alert('data submitted, wait for display');
         $("#out").text(JSON.stringify(data));  //in array, data[i] for each item
-        
+	*/
+	    console.log(data)
+
+	    var svg = d3.select('svg')
+	    	.attr("width", "100%")
+	    	.attr("height", "100%")
+
+	    var usages = [];
+	    for(var j = 0; j < data.length; j++){
+		    usages[j] = data[j].USAGE;
+	    }
+	    console.log(usages);
+
+	    svg.selectAll("rect")
+	    	.data(usages)
+	    	.enter().append("rect")
+	    		.attr("height", function(d,i) {return (d * 10)})
+	    		.attr("width","40")
+	    		.attr("x", function(d,i) {return i * 60 + 25})
+	    		.attr("y", function(d,i) {return(400 - d * 10)});
+
+
+
     }).catch( function(error){
         alert('error' + error);
     });
