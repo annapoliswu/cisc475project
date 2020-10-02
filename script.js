612bd8f476648b512395e567fa706b18d583cdc6
David Boulden <53497062+daveb4@users.noreply.github.com>
2020-10-02 11:31:23 -0400

    removed comments

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 $("#submitButton").click(function () {
     let fileURL =  URL.createObjectURL($('#csvData').prop('files')[0]); //get file and then create temporary url for d3 to read
 
-    /*
-    var csv = URL.createObjectURL(new Blob([
-    `12,43,21
-    45,54,21
-    87,13,17
-    98,69,17`
-    ]));
-    */
-
     d3.csv(fileURL).then(function(data) {
-	/*
-        alert('data submitted, wait for display');
-        $("#out").text(JSON.stringify(data));  //in array, data[i] for each item
-	*/
 	    console.log(data)
 
 	    var svg = d3.select('svg')
@@ -36,8 +23,6 @@ $("#submitButton").click(function () {
 	    		.attr("width","40")
 	    		.attr("x", function(d,i) {return i * 60 + 25})
 	    		.attr("y", function(d,i) {return(800 - (d * 1000))});
-
-
 
     }).catch( function(error){
         alert('error' + error);
