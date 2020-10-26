375af00d1f19b48382f8a60bb61a9113e2af9bae
vincentb <vincentb@udel.edu>
2020-10-26 16:22:48 -0400

    No repeating graphs

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var startDate;
 var endDate;
+var firstClick = false;
+var svg = 0;
+var margin = {};
+var width = 0;
+var height = 0;
+var left = 0;
+var bottom = 0;
 
 //every time start date changes, update dateEntered value
 $('#start').on("change", function () {
 	var inputStart = this.value;
-	if(inputStart != null){
+	if (inputStart != null) {
 		startDate = new Date(inputStart);
 	}
 	console.log(inputStart);
@@ -13,49 +20,62 @@ $('#start').on("change", function () {
 
 $('#end').on("change", function () {
 	var inputEnd = this.value;
-	if(inputEnd != null){
+	if (inputEnd != null) {
 		endDate = new Date(inputEnd);
 	}
 	console.log(inputEnd);
 	console.log(endDate);
 });
 
-$("#lineGraph").click(function(e){
+$("#lineGraph").click(function (e) {
 	$("#pickGraphStyle").text("Line Graph");
 });
-$("#barGraph").click(function(e){
+$("#barGraph").click(function (e) {
 	$("#pickGraphStyle").text("Bar Graph");
 });
 
 //on submit, read data, set domain range, append graph
 $("#submitButton").click(function () {
+	if ($('#csvData').prop('files').length > 0) {
+		let fileURL = URL.createObjectURL($('#csvData').prop('files')[0]); //get file and then create temporary url for d3 to read
 
-	if ($('#pickGraphStyle').text() == "Bar Graph"){
-		makeBarGraph();
-	} else {
-		makeLineGraph();
-	}
-	
-   
-});
-function makeLineGraph(){
-	//get file and then create temporary url for d3 to read
-	let fileURL = URL.createObjectURL($('#csvData').prop('files')[0]);
-
-	//specify height and width
-	var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-		width = 1200 - margin.left - margin.right, //was 600
+		//specify height and width
+		margin = { top: 10, right: 30, bottom: 30, left: 60 };
+		width = 1200 - margin.left - margin.right; //was 600
 		height = 400 - margin.top - margin.bottom;
 
-	// append the svg object in #graph
-	var svg = d3.select("#graph")
-		.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform",
-			"translate(" + margin.left + "," + margin.top + ")");
+		if (!firstClick) {
+			svg = d3.select("#graph")
+				.append("svg")
+				.attr("width", width)
+				.attr("height", height + margin.top + margin.bottom)
+				.append("g")
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			firstClick = true;
+			console.log(svg.html());
+		} else {
+			for (i = 0; i < 3; i++) {
+				elt = d3.select("#graph g")
+				console.log(d3.select("#graph").html());
+			}
+			elt = d3.select("#graph svg")
+			elt.remove();
+			svg = d3.select("#graph")
+				.append("svg")
+				.attr("width", width)
+				.attr("height", height + margin.top + margin.bottom)
+				.append("g")
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		}
+		if ($('#pickGraphStyle').text() == "Bar Graph") {
+			makeBarGraph(fileURL);
+		} else {
+			makeLineGraph(fileURL);
+		}
+	}
+});
 
+function makeLineGraph(fileURL) {
 	//d3.csv(URL, function to format data, function to use data)
 	d3.csv(fileURL,
 		function (d) {
@@ -64,17 +84,17 @@ function makeLineGraph(){
 				d3.timeparse puts into a Date object that d3 can use to auto scale
 				%H is on 24 hr time, %I is 12 hr
 			*/
-			return { test: d3.timeParse("%m/%d/%Y")(d.DATE), date: d3.timeParse("%m/%d/%Y-%H:%M")(d.DATE + "-" + d['END TIME']), value: d.USAGE }
+			return { date: d3.timeParse("%m/%d/%Y-%H:%M")(d.DATE + "-" + d['END TIME']), value: d.USAGE }
 		},
 
 		function (data) {
 
 			//check for date input so that date is valid
 			//extent(array) returns [min, max] 
-			let minmax = d3.extent(data, function (d) { return d.date; }); 
+			let minmax = d3.extent(data, function (d) { return d.date; });
 			let min = minmax[0];
 			let max = minmax[1];
-			console.log(minmax);
+			//console.log(minmax);
 
 			if (startDate == null || startDate < min || startDate > max) {
 				startDate = min;
@@ -87,18 +107,20 @@ function makeLineGraph(){
 			var x = d3.scaleTime()
 				.domain([startDate, endDate])  	//domain takes in [min, max] and specifies the range the graph shows
 				.range([0, width]);
-			svg.append("g")
+			//console.log(svg.html());
+			bottom = svg.append("g")
 				.attr("transform", "translate(0," + height + ")")
 				.call(d3.axisBottom(x));
+			//console.log(svg.html());
 
 			// y axis
 			var y = d3.scaleLinear()
 				.domain([0, d3.max(data, function (d) { return +d.value; })])
 				.range([height, 0]);
-			svg.append("g")
+			left = svg.append("g")
 				.call(d3.axisLeft(y));
 
-				
+
 			// line and line styles
 			svg.append("path")
 				.datum(data)
@@ -106,36 +128,23 @@ function makeLineGraph(){
 				.attr("stroke", "blue")     //line color
 				.attr("stroke-width", 1.5)
 				.attr("d", d3.line()
-					.defined(function(d) { 
-						return d.date < endDate && d.date > startDate; 
+					.defined(function (d) {
+						return d.date < endDate && d.date > startDate;
 					})
 					.x(function (d) { return x(d.date) })
 					.y(function (d) { return y(d.value) })
 				).attr("class", "graphline")    // so we can css select .graphline for further styling
 
 		});
+
 }
-function makeBarGraph(){
-	let fileURL =  URL.createObjectURL($('#csvData').prop('files')[0]); //get file and then create temporary url for d3 to read
-
-	var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-		width = 1000 - margin.left - margin.right,
-		height = 400 - margin.top - margin.bottom;
-
-	var svg = d3.select("#graph")
-		.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform",
-			"translate(" + margin.left + "," + margin.top + ")");
-
+function makeBarGraph(fileURL) {
 	d3.csv(fileURL,
-		function(d) {
+		function (d) {
 			return { date: d3.timeParse("%m/%d/%Y-%H:%M")(d.DATE + "-" + d['END TIME']), value: d.USAGE }
 		},
 
-		function(data) {
+		function (data) {
 			var x = d3.scaleTime()
 				.domain(d3.extent(data, function (d) { return d.date; }))
 				.range([0, width]);
@@ -153,10 +162,10 @@ function makeBarGraph(){
 				.data(data)
 				.enter().append("rect")
 				.attr("class", "bar")
-				.attr("x", function(d) { return x(d.date); })
-				.attr("y", function(d) { return y(d.value); })
-				.attr("width", width/data.length)
-				.attr("height", function(d) { return height - y(d.value); });
+				.attr("x", function (d) { return x(d.date); })
+				.attr("y", function (d) { return y(d.value); })
+				.attr("width", width / data.length)
+				.attr("height", function (d) { return height - y(d.value); });
 
 		});
 }
